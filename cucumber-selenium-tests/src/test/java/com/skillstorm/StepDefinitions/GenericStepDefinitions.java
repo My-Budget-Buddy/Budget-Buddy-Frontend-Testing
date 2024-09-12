9bbe4fb6c951ad0689d0968ac2ceed8a80f50188
Bryar-Frank <54441650+Bryar-Frank@users.noreply.github.com>
2024-09-12 10:57:20 -0400

    initial creation of cucumber folder layout

---
+++ cucumber-selenium-tests/src/test/java/com/skillstorm/StepDefinitions/GenericStepDefinitions.java
@@ -0,0 +1,19 @@
+package com.skillstorm.StepDefinitions;
+
+
+
+public class GenericStepDefinitions {
+
+    // @Given("an example scenario")
+    // public void anExampleScenario() {
+    // }
+
+    // @When("all step definitions are implemented")
+    // public void allStepDefinitionsAreImplemented() {
+    // }
+
+    // @Then("the scenario passes")
+    // public void theScenarioPasses() {
+    // }
+
+}
