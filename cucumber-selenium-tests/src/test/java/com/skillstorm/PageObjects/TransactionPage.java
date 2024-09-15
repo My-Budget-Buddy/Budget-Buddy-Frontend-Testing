5ef5011375ab0e1753505e97e74a7a77b43106cf
jmensah365 <jmensah365@gmail.com>
2024-09-15 14:13:32 -0400

    updating POM and SD files

--- cucumber-selenium-tests/src/test/java/com/skillstorm/PageObjects/TransactionPage.java
+++ cucumber-selenium-tests/src/test/java/com/skillstorm/PageObjects/TransactionPage.java
@@ -1,88 +1,276 @@
 package com.skillstorm.PageObjects;
 
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class TransactionPage {
-    //get all locators
+    
+    WebDriver driver;
+    private static final String transactionsUrl = "http://localhost:5173/dashboard/transactions";
 
-    //finding transactions page title
+    // Locators for Transaction Page Elements
     @FindBy(className = "usa-logo__text")
     private WebElement transactionPageTitle;
 
-    //finding clear filter button
     @FindBy(id = "clearFilterBtn")
     private WebElement clearFilterBtn;
 
-    //finding sort by date/amount dropdown
     @FindBy(id = "sortByDropdown")
     private WebElement sortByDropdown;
 
-    //finding descending/ascending dropdown
     @FindBy(id = "directionDropdown")
     private WebElement directionDropdown;
 
-    /*
-     * finding add transaction button, which actually just triggers a modal
-    */ 
     @FindBy(id = "addTransactionModal")
     private WebElement addTransactionModal;
 
-    //finding submit button
+    // Modal Fields
+    @FindBy(name = "vendorName")
+    private WebElement vendorNameField;
+
+    @FindBy(name = "accountId")
+    private WebElement accountDropdown;
+
+    @FindBy(name = "amount")
+    private WebElement amountField;
+
+    @FindBy(name = "category")
+    private WebElement categoryDropdown;
+
     @FindBy(id = "addTransactionBtn")
     private WebElement submitBtn;
 
-    //finding all categories dropdown
+    // Filter Dropdowns
     @FindBy(id = "allCategoriesDropDown")
     private WebElement allCategoriesDropDown;
 
-    //finding all accounts dropdown
     @FindBy(id = "allAccountDropDown")
     private WebElement allAccountDropDown;
 
-    //finding all amounts dropdown
     @FindBy(id = "allAmountsDropDown")
     private WebElement allAmountsDropDown;
 
-    //finding all dates dropdown
     @FindBy(id = "allDatesDropDown")
     private WebElement allDatesDropDown;
 
-    //finding transaction table title
+    // Transaction Table Elements
     @FindBy(id = "listOfTransactionsTitle")
     private WebElement transactionsTableTitle;
 
-    //finding transactions table
-    @FindBy(className = "usa-table usa-table--borderless width full")
+    @FindBy(xpath = "//*[@id=\"root\"]/div[1]/main/div/div[3]/div/table")
     private WebElement transactionsTable;
 
-    
-    //finding date header
+    @FindBy(xpath = "//*[@id=\"root\"]/div[1]/main/div/div[3]/div/table/tbody/tr[1]")
+    private WebElement transactionsTableFirstRow;
+
+    // Table Headers
     @FindBy(xpath = "//*[@id=\"root\"]/div[1]/main/div/div[3]/div/table/thead/tr/th[1]")
     private WebElement dateHeader;
 
-    //finding name header
     @FindBy(xpath = "//*[@id=\"root\"]/div[1]/main/div/div[3]/div/table/thead/tr/th[2]")
     private WebElement nameHeader;
 
-    //finding category header
     @FindBy(xpath = "//*[@id=\"root\"]/div[1]/main/div/div[3]/div/table/thead/tr/th[3]")
     private WebElement categoryHeader;
 
-    //finding actions header
     @FindBy(xpath = "//*[@id=\"root\"]/div[1]/main/div/div[3]/div/table/thead/tr/th[4]")
     private WebElement actionsHeader;
 
-    //finding amount header
     @FindBy(xpath = "//*[@id=\"root\"]/div[1]/main/div/div[3]/div/table/thead/tr/th[5]")
     private WebElement amountHeader;
 
-    //finding edit icon
+    // Edit Transaction Elements
     @FindBy(id = "editBtn")
     private WebElement editBtn;
 
-    //finding delete icon
+    @FindBy(id = "editTransactionBtn")
+    private WebElement editSubmitBtn;
+
+    @FindBy(id = "edit-transaction-vendorName")
+    private WebElement editTransactionNameField;
+
+    @FindBy(id = "edit-transaction-account")
+    private WebElement editTransactionAccountField;
+
+    @FindBy(id = "edit-transaction-amount")
+    private WebElement editTransactionAmountField;
+
+    @FindBy(id = "edit-transaction-category")
+    private WebElement editTransactionCategoryField;
+
+    // Delete Transaction Elements
     @FindBy(id = "deleteBtn")
     private WebElement deleteBtn;
 
+    // Login Elements
+    @FindBy(name = "email")
+    private WebElement emailField;
+
+    @FindBy(name = "password")
+    private WebElement passwordField;
+
+    @FindBy(id = "logInBtn")
+    private WebElement logInBtn;
+
+    @FindBy(xpath = "//*[@id=\"root\"]/div[1]/div/div[1]/a[5]")
+    private WebElement transactionsTab;
+
+    private int tableSizeBeforeDeletion = 0;
+
+    // Constructor
+    public TransactionPage(WebDriver driver) {
+        this.driver = driver;
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        PageFactory.initElements(driver, this);
+    }
+
+    // Quit driver
+    public void quit() {
+        this.driver.quit();
+    }
+
+    // Login Methods
+    public void loginPage() {
+        this.driver.get("http://localhost:5173/login");
+    }
+
+    public void logIn() {
+        emailField.sendKeys("joseph.sam@gmail.com");
+        passwordField.sendKeys("password1");
+        logInBtn.click();
+    }
+
+    public void clickTab() {
+        transactionsTab.click();
+    }
+
+    // Navigation Methods
+    public void getTransactionsUrl() {
+        this.driver.navigate().to(transactionsUrl);
+    }
+
+    //General methods
+    public String getCurrentUrl() {
+        return this.driver.getCurrentUrl();
+    }
+
+    public void clickSubmitBtn() {
+        submitBtn.click();
+    }
+
+    // Create Transaction Methods
+    public void triggerAddTransactionModal() {
+        addTransactionModal.click();
+    }
+
+    public String setName(String name) {
+        vendorNameField.sendKeys(name);
+        return name;
+    }
+
+    public String setAccount(String account) {
+        if (account != null && !account.isEmpty()) {
+            accountDropdown.click();
+            WebElement option = driver.findElement(By.xpath("//*[@id='transaction-account']/option[text()='" + account + "']"));
+            option.click();
+        }
+        return account;
+    }
+
+    public String setAmount(String amount) {
+        amountField.clear();
+        amountField.sendKeys(amount);
+        return amount;
+    }
+
+    public String setCategory(String category) {
+        if (category != null && !category.isEmpty()) {
+            accountDropdown.click();
+            WebElement option = driver.findElement(By.xpath("//*[@id='transaction-category']/option[text()='" + category + "']"));
+            option.click();
+        }
+        return category;
+    }
+
+    public String verifyTransactionDetails() {
+        return transactionsTableFirstRow.getText();
+    }
+
+    // Read Transaction Methods
+    public Boolean printTransactionTable() {
+        System.out.println(transactionsTable.getText());
+        return transactionsTable.isDisplayed();
+    }
+
+    // Update Transaction Methods
+    public void clickEditIcon() {
+        editBtn.click();
+    }
+
+    public void updateName(String name) {
+        editTransactionNameField.clear();
+        editTransactionNameField.sendKeys(name);
+    }
+
+    public void updateAccount(String account) {
+        if (account != null && !account.isEmpty()) {
+            editTransactionAccountField.click();
+            WebElement option = driver.findElement(By.xpath("//*[@id='edit-transaction-account']/option[text()='" + account + "']"));
+            option.click();
+        }
+    }
+
+    public void updateAmount(String amount) {
+        editTransactionAmountField.clear();
+        editTransactionAmountField.sendKeys(amount);
+    }
+
+    public void updateCategory(String category) {
+        if (category != null && !category.isEmpty()) {
+            editTransactionCategoryField.click();
+            WebElement option = driver.findElement(By.xpath("//*[@id='edit-transaction-category']/option[text()='" + category + "']"));
+            option.click();
+        }
+    }
+
+    public void clickEditSubmitBtn() {
+        editSubmitBtn.click();
+    }
+
+    // Delete Transaction Methods
+    public void clickDeleteBtn() {
+        tableSizeBeforeDeletion = transactionsTable.getText().length();
+        deleteBtn.click();
+    }
+
+    public Boolean confirmDeletion() {
+        int tableSizeAfterDeletion = transactionsTable.getText().length();
+        return tableSizeBeforeDeletion > tableSizeAfterDeletion;
+    }
+
+    // Category Methods
+    public void clickAllCategories() {
+        allCategoriesDropDown.click();
+    }
+
+    public void selectACategory(String category) {
+        if (category != null && !category.isEmpty()) {
+            accountDropdown.click();
+            WebElement option = driver.findElement(By.xpath("//*[@id='allCategoriesDropDown']/option[text()='" + category + "']"));
+            option.click();
+        }
+    }
+
+    public void printOutCategoryColumn() {
+        List<WebElement> categoryColumnValues = driver.findElements(By.xpath("//*[@id='root']/div[1]/main/div/div[3]/div/table//tr/td[2]"));
+        for (WebElement cell : categoryColumnValues) {
+            System.out.println(cell.getText());
+        }
+    }
 }
--- cucumber-selenium-tests/src/test/java/com/skillstorm/StepDefinitions/SDTransactions.java
+++ cucumber-selenium-tests/src/test/java/com/skillstorm/StepDefinitions/SDTransactions.java
@@ -2,90 +2,150 @@ package com.skillstorm.StepDefinitions;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
+import org.testng.Assert;
 import com.skillstorm.PageObjects.TransactionPage;
-
 import org.openqa.selenium.WebDriver;
-
+import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.*;
 
 public class SDTransactions {
-    private TransactionPage transactions;
 
+    private TransactionPage transactions;
+    private String[] transactionContent;
+
+    /**
+     * Set up the ChromeDriver and initialize TransactionPage.
+     */
     @Before("@transaction")
-    public void before(){
+    public void before() {
         ChromeOptions options = new ChromeOptions();
         WebDriver driver = new ChromeDriver(options);
+
+        this.transactions = new TransactionPage(driver);
+
+        //storing the content of the transaction for later testing
+        transactionContent = new String[4];
     }
 
+    /**
+     * Tear down the WebDriver after each test.
+     */
+    @After("@transaction")
+    public void after() {
+        this.transactions.quit();
+    }
 
-    //start general SD
+    /**
+     * Login Scenario Definitions
+     */
+    @Given("I am on the login page")
+    public void logInPage() {
+        this.transactions.loginPage();
+    }
+
+    @And("I login")
+    public void login() {
+        this.transactions.logIn();
+    }
+
+    /**
+     * General Scenario Definitions
+     */
     @Given("I am on the Transactions page")
-    public void iAmOnTheTranactionsPage(){
+    public void iAmOnTheTranactionsPage() {
+        this.transactions.clickTab();
+        String expectedUrl = "http://localhost:5173/dashboard/transactions";
+        String actualUrl = this.transactions.getCurrentUrl();
+        Assert.assertEquals(actualUrl, expectedUrl);
     }
-
 
     @And("I click the Submit button")
-    public void iClickTheSubmitButton(){}
-    //end general SD
+    public void iClickTheSubmitButton() {
+        this.transactions.clickSubmitBtn();
+    }
 
-
-    
-    //start create
+    /**
+     * Create Transaction Scenario Definitions
+     */
     @When("I click the Add Transactions button")
-    public void iClickTheAddTransactionButton(){}
+    public void iClickTheAddTransactionButton() {
+        this.transactions.triggerAddTransactionModal();
+    }
 
-    @And("And I fill in the {string}, {string}, {string}, and {string}")
-    public void andIFillInThe(String name, String account, String amount, String category){}
+    @And("I fill in the {string}, {string}, {string}, and {string}")
+    public void andIFillInThe(String name, String account, String amount, String category) {
+        transactionContent[0] = this.transactions.setName(name);
+        transactionContent[1] = this.transactions.setAccount(account);
+        transactionContent[2] = this.transactions.setAmount(amount);
+        transactionContent[3] = this.transactions.setCategory(category);
+    }
 
     @Then("I can see the new transaction in my list")
-    public void iCanSeeTheNewTransactionInMyList(){}
-    //end create
+    public void iCanSeeTheNewTransactionInMyList() {
+        String actualTransaction = this.transactions.verifyTransactionDetails();
+        System.out.println(actualTransaction);
+    }
 
-
-
-    //start read
+    /**
+     * Read Transaction Scenario Definitions
+     */
     @Then("I can see the list of all my transactions")
-    public void iCanSeeTheListOfAllMyTransactions(){}
-    //end read
+    public void iCanSeeTheListOfAllMyTransactions() {
+        this.transactions.printTransactionTable();
+        Assert.assertTrue(this.transactions.printTransactionTable());
+    }
 
-    
-
-    //start update
+    /**
+     * Update Transaction Scenario Definitions
+     */
     @When("I click the Pencil Icon button")
-    public void iClickThePencilIconButton(){}
+    public void iClickThePencilIconButton() {
+        this.transactions.clickEditIcon();
+    }
 
-    @And("And I update the {string}, {string}, {string}, and {string}")
-    public void andIUpdateThe(String name, String account, String amount, String category){}
+    @And("I update the {string}, {string}, {string}, and {string}")
+    public void andIUpdateThe(String name, String account, String amount, String category) {
+        this.transactions.updateName(name);
+        this.transactions.updateAccount(account);
+        this.transactions.updateAmount(amount);
+        this.transactions.updateCategory(category);
+    }
 
-    @Then ("I can see the updated transaction in my list")
-    public void iCanSeeTheUpdatedTransactionInMyList(){}
-    //end update
+    @And("I click the edit Submit button")
+    public void iClickTheEditSubmitButton() {
+        this.transactions.clickEditSubmitBtn();
+    }
 
-
-
-    //start delete
+    /**
+     * Delete Transaction Scenario Definitions
+     */
     @When("I click the Trash Icon button")
-    public void iClickTheTrashIconButton(){
+    public void iClickTheTrashIconButton() {
+        this.transactions.clickDeleteBtn();
     }
 
     @Then("the transaction is not in the list")
-    public void theTransactionIsNotInTheList(){}
-    //end delete
+    public void theTransactionIsNotInTheList() {
+        this.transactions.confirmDeletion();
+        Assert.assertTrue(this.transactions.confirmDeletion());
+    }
 
-
-
-    //start category
+    /**
+     * Category Filter Scenario Definitions
+     */
     @When("I click the All Categories button")
-    public void iClickTheAllCategoriesButton(){}
+    public void iClickTheAllCategoriesButton() {
+        this.transactions.clickAllCategories();
+    }
 
     @And("I click a {string} to filter based on")
-    public void iClickToFilterBasedOn(String category){}
+    public void iClickToFilterBasedOn(String category) {
+        this.transactions.selectACategory(category);
+    }
 
     @Then("only transactions with that category should be visible")
-    public void onlyTransactionsWithThatCategoryShouldBeVisible(){}
-    //end category
-
-
+    public void onlyTransactionsWithThatCategoryShouldBeVisible() {
+        this.transactions.printOutCategoryColumn();
+    }
 }
