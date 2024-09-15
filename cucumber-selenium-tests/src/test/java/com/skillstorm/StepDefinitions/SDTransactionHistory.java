1f2f69c5d7b1fe36e94c612316990e9642ebe5a2
jmensah365 <jmensah365@gmail.com>
2024-09-15 14:13:32 -0400

    updating POM and SD files

--- cucumber-selenium-tests/src/test/java/com/skillstorm/StepDefinitions/SDTransactionHistory.java
+++ cucumber-selenium-tests/src/test/java/com/skillstorm/StepDefinitions/SDTransactionHistory.java
@@ -4,13 +4,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import com.skillstorm.PageObjects.TransactionHistory;
+import com.skillstorm.PageObjects.TransactionHistoryPage;
 
 import io.cucumber.java.Before;
 import io.cucumber.java.en.*;
 
 public class SDTransactionHistory {
-    private TransactionHistory transactionHistory;
+    private TransactionHistoryPage transactionHistory;
     
     
     @Before("@transactionhistory")
@@ -23,8 +23,8 @@ public class SDTransactionHistory {
     @Given("I am on the Transaction History page")
     public void iAmOnTheTransactionHistoryPage(){}
 
-    @And("I click the Submit button")
-    public void iClickTheSubmitButton(){}
+    // @And("I click the Submit button")
+    // public void iClickTheSubmitButton(){}
     //end general
 
 
@@ -50,14 +50,14 @@ public class SDTransactionHistory {
 
     //start create
 
-    @When("I click the Add Transactions button")
-    public void iClickTheAddTransactionButton(){}
+    // @When("I click the Add Transactions button")
+    // public void iClickTheAddTransactionButton(){}
 
-    @And("I fill in the {string}, {string}, {string}, and {string}")
-    public void iFillInThe(String name, String account, String amount, String category){}
+    // @And("I fill in the {string}, {string}, {string}, and {string}")
+    // public void iFillInThe(String name, String account, String amount, String category){}
 
-    @Then("I can see the new transaction in my list")
-    public void iCanSeeTheNewTransactionInMyList(){}
+    // @Then("I can see the new transaction in my list")
+    // public void iCanSeeTheNewTransactionInMyList(){}
 
     //end create
 
@@ -67,5 +67,32 @@ public class SDTransactionHistory {
     public void iCanSeeTheListOfAllMyPastTransactions(){}
     //end read
 
+    //start update
+    // @When("I click on a transaction in the graph view")
+    // public void iClickOnATransactionInTheGraphView(){}
 
+    // @And("I click the Edit Transaction button")
+    // public void iClickTheEditTransactionButton(){}
+
+    // @And ("I update the {string}, {string}, {string}, and {string}")
+    // public void iUpdateThe(){}
+
+    // @Then("I can see the updated transaction in my list")
+    // public void iCanSeeTheUpdatedTransactionInMyList(){}
+
+    //end update
+
+    //start delete
+    // @When("I click the Trash Icon button")
+    // public void iClickTheTrashIconButton(){
+    // }
+
+    // @Then("the transaction is not in the list")
+    // public void theTransactionIsNotInTheList(){}
+    //end delete
+
+    //start graphical summary
+    @Then("I can see a graphical summary of my transaction history")
+    public void iCanSeeAGraphicalSummaryOfMyTransactionHistory(){}
+     //end graphical summary
 }
