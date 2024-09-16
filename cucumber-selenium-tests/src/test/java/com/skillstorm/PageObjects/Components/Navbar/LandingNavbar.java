99a321838d836a8366ce705ccbb2951dffe15e14
Aaron Huggins <achuggi1@asu.edu>
2024-09-16 12:38:47 -0700

    LandingNavbar
    Added a logged in/out status to the Landing Navbar to make it more interactable, also filled out static fields.

--- cucumber-selenium-tests/src/test/java/com/skillstorm/PageObjects/Components/Navbar/LandingNavbar.java
+++ cucumber-selenium-tests/src/test/java/com/skillstorm/PageObjects/Components/Navbar/LandingNavbar.java
@@ -10,6 +10,8 @@ package com.skillstorm.PageObjects.Components.Navbar;
 import java.util.Arrays;
 import java.util.List;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
@@ -19,18 +21,18 @@ import com.skillstorm.Utilities.UserData.LoginStatus;
 public class LandingNavbar extends Navbar{
 //#region Static fields
     // IDs
-    public static final String BTN_LANDING_ID = "";
-    public static final String BTN_LOGIN_ID = "";
-    public static final String BTN_REGISTER_ID = "";
-    public static final String BTN_DASHBOARD_ID = "";
-    public static final String BTN_LOGOUT_ID = "";
+    public static final String BTN_LANDING_ID = "landing-link";
+    public static final String BTN_LOGIN_ID = "login-link";
+    public static final String BTN_REGISTER_ID = "register-link";
+    public static final String BTN_DASHBOARD_ID = "dashboard-link";
+    public static final String BTN_LOGOUT_ID = "logout-button";
 
     // Names
     public static final String BTN_LANDING_NAME = "Navbar Budget Buddy Icon";
     public static final String BTN_LOGIN_NAME = "Navbar Log In";
     public static final String BTN_REGISTER_NAME = "Navbar Register";
-    public static final String BTN_DASHBOARD_NAME = "";
-    public static final String BTN_LOGOUT_NAME = "";
+    public static final String BTN_DASHBOARD_NAME = "Navbar Dashboard";
+    public static final String BTN_LOGOUT_NAME = "Navbar Logout";
 //#endregion
 
     private LoginStatus loginStatus;
@@ -88,16 +90,20 @@ public class LandingNavbar extends Navbar{
      */
     @Override
     public WebElement getWebElement(String name) {
-        switch (name) {
-            case BTN_LANDING_NAME:
-                return btnLanding;
-            case BTN_LOGIN_NAME:
-                return btnLogin;
-            case BTN_REGISTER_NAME:
-                return btnRegister;
-            default:
-                throw new IllegalArgumentException("Button '" + name + "' does not exist.");
+        checkLoginStatus();
+        if(name.equals(BTN_LANDING_NAME)) return btnLanding;
+
+        if(loginStatus == LoginStatus.LoggedIn){
+            if(name.equals(BTN_DASHBOARD_NAME)) return btnDashboard;
+            if(name.equals(BTN_LOGOUT_NAME)) return btnLogout;
+        }else if(loginStatus == LoginStatus.LoggedOut){
+            if(name.equals(BTN_LOGIN_NAME)) return btnLogin;
+            if(name.equals(BTN_REGISTER_NAME)) return btnRegister;
+        }else{
+            throw new RuntimeException("Login status was Unknown.");
         }
+
+        return null;
     }
 
     /**
@@ -105,6 +111,30 @@ public class LandingNavbar extends Navbar{
      */
     @Override
     public List<WebElement> getButtons() {
-        return Arrays.asList(btnLanding, btnLogin, btnRegister);
+        checkLoginStatus();
+        return (loginStatus == LoginStatus.LoggedIn ? 
+            Arrays.asList(btnLanding, btnLogin, btnRegister) :
+            Arrays.asList(btnLanding, btnDashboard, btnLogout));
+    }
+
+    /**
+     * Sets the login status and loads appropriate elements.
+     */
+    private void checkLoginStatus(){
+        loginStatus = LoginStatus.Unknown;  // In case something crazy goes wrong.
+
+        btnLanding = driver.findElement(By.id(BTN_LANDING_ID));
+
+        try{
+            // Logged in
+            btnLogin = driver.findElement(By.id(BTN_LOGIN_ID));
+            btnRegister = driver.findElement(By.id(BTN_REGISTER_ID));
+            loginStatus = LoginStatus.LoggedOut;
+        }catch(NoSuchElementException e){
+            // Logged out.
+            btnDashboard = driver.findElement(By.id(BTN_DASHBOARD_ID));
+            btnLogout = driver.findElement(By.id(BTN_LOGOUT_ID));
+            loginStatus = LoginStatus.LoggedIn;
+        }
     }
 }
