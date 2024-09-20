f69f01f974f10ae8b0b6c198b4c5749a2d988a05
Anthowu07 <anthony07wu@gmail.com>
2024-09-20 10:48:11 -0400

    adjust url for error page in Navigator.java

--- cucumber-selenium-tests/src/test/java/com/skillstorm/Utilities/Navigator.java
+++ cucumber-selenium-tests/src/test/java/com/skillstorm/Utilities/Navigator.java
@@ -48,7 +48,7 @@ public class Navigator {
     public static final String URL_TAX= "";
     public static final String URL_TAXEDITVIEW= "";
     public static final String URL_TAXRESULTS= "";
-    public static final String URL_ERROR= "http://localhost:5173/error";
+    public static final String URL_ERROR= URL + "/error";
 //#endregion
 
     Map<String, String> pageUrlMap = new HashMap<>();
